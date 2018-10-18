aa7356f4215604693e9b68dd155c772f6fe66ee8
Phillip Cunningham <phillipcunningham@Phillips-MacBook-Pro.local>
2018-10-18 18:35:05 -0500

    Added dog control model:

--- resources/views/layouts/create.blade.php
+++ resources/views/layouts/create.blade.php
@@ -1,14 +1,13 @@
-@extends('layouts/app')
+@extends('layouts.app')
 
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header"><strong>This is a test</strong></div>
-                    <p>This is a test</p>
+                    <div class='card-header'>Add an entry!</div>
                 </div>
             </div>
         </div>
     </div>
-@endsection()
+    @endsection
